0a4abf2358f56cbc9f5b9518e3523a7c0268ec17
Sergey Kuksov <sergey.kuksov@func.ru>
2019-03-25 15:45:43 +0300

    Исправил иконки табов

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <ion-app>
+
     <ion-tabs>
-      <ion-tab tab="flash">
+      <ion-tab tab="flash" icon="flash" label="flash">
         <ion-header>
           <ion-toolbar>
             <ion-title>Header</ion-title>
@@ -10,7 +11,16 @@
 
         <router-view/>
       </ion-tab>
-      <ion-tab tab="apps">
+      <ion-tab tab="apps" icon="apps" label="apps">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Header</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-icon name="flash"></ion-icon>
+      </ion-tab>
+      <ion-tab tab="send" icon="send" label="send">
         <ion-header>
           <ion-toolbar>
             <ion-title>Header</ion-title>
@@ -20,33 +30,22 @@
         <ion-icon name="flash"></ion-icon>
       </ion-tab>
 
-      <ion-tab tab="send">
-        <ion-header>
-          <ion-toolbar>
-            <ion-title>Header</ion-title>
-          </ion-toolbar>
-        </ion-header>
-
-        <ion-icon name="flash"></ion-icon>
-      </ion-tab>
-
-      <ion-tab-bar slot="bottom">
+      <!-- <ion-tab-bar slot="bottom">
         <ion-tab-button tab="flash">
-          <ion-icon name="flash"></ion-icon>
-          <!-- <ion-icon name="flash"></ion-icon>
-          <ion-label>Tab One</ion-label>-->
+          <ion-icon name="flash" layout="icon-hide"></ion-icon>
+          <ion-label>Tab One</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="apps">
-          <!-- <ion-icon name="apps"></ion-icon>
-          <ion-label>Tab Two</ion-label>-->
+          <ion-icon name="apps"></ion-icon>
+          <ion-label>Tab Two</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="send">
-          <!-- <ion-icon name="send"></ion-icon>
-          <ion-label>Tab Three</ion-label>-->
+          <ion-icon name="send"></ion-icon>
+          <ion-label>Tab Three</ion-label>
         </ion-tab-button>
-      </ion-tab-bar>
+      </ion-tab-bar> -->
     </ion-tabs>
   </ion-app>
 </template>
